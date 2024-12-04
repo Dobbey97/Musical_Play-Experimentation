3f35fb19f14a135e25e944af1c1bcf6c40c3f520
Dean <C24732491@myTUDublin.ie>
2024-12-04 00:50:13 +0000

    Sorted Files + Added Volume Control

--- musical-play-&-experimentation/control.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-@export var keybind = "1"
-@export var sound: Control
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed(keybind):
-		$"Sound 1".play()
